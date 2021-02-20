04ff533f5085318ad0400aa0116d0fc55afc5c3b
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-02-20 10:45:20 +0100

    Nested Data and Additional Exploration

--- iteration.js
+++ iteration.js
@@ -1,25 +1,32 @@
-/*
-const userNames = ['Samir', 'Angela', 'Beatrice', 'Shaniqua', 'Marvin', 'Sean'];
-    // Result: [{name: 'Samir'}, {name: 'Shaniqua'}, {name:'Sean'}];
-
-const users = userNames
-  .filter(nome => nome.charAt(0) === 'S')
-  .map(nome => ({nome}));
-
-console.log(users);
-*/
-
 const users = [
-{nome: 'Samir', idade: 27},
-{nome: 'Angela', idade: 33},
-{nome: 'Beatriz', idade: 42},
-{nome: 'Shaniqua', idade: 30},
-{nome: 'Marvin', idade: 23},
-{nome: 'Sean', idade: 47}
+  {
+    name: 'Samir',
+    age: 27,
+    favoriteBooks:[
+      {title: 'The Iliad'},
+      {title: 'The Brothers Karamazov'}
+    ]
+  },
+  {
+    name: 'Angela',
+    age: 33,
+    favoriteBooks:[
+      {title: 'Tenth of December'},
+      {title: 'Cloud Atlas'},
+      {title: 'One Hundred Years of Solitude'}
+    ]
+  },
+  {
+    name: 'Beatrice',
+    age: 42,
+    favoriteBooks:[
+      {title: 'Candide'}
+    ]
+  }
 ];
 
-const userNames = users
-  .filter(user => user.idade >= 30)
-  .map(user => user.nome);
+const livros = users 
+  .map(user => user.favoriteBooks.map(book => book.title))
+  .reduce((arr, titles) => [...arr,...titles], []);
 
-console.log(userNames);
+console.log(livros);
